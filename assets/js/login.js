f4e68d5bb884b2498fb39f34d5562b33eb2387fd
Ranran <Ranran@qq.com>
2020-06-21 10:15:31 +0800

    登录注册-自定义校验规则

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,18 @@ $(function(){
         $('.login-box').show();
         $('.reg-box').hide();
     })
+    // 自定义验证
+    var form=layui.form;
+    form.verify({
+        pass: [
+            /^[\S]{6,12}$/
+            ,'密码必须6到12位，且不能出现空格'
+          ] ,
+          repass:(function(value){
+            var pass=$('#pwd').val();
+            if(pass!==value){
+                return "两次输入的密码不一致"
+            }
+          })
+    })
 })
